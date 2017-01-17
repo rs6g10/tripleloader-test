9b12232ce82bceb67b68fd11647851cebc36f55e
Rahul Sharma <rahuldsharma@hotmail.co.uk>
2017-01-17 00:19:54 +0000

    initial commit

---
+++ DataLoader/Association.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DataLoader
+{
+    public class Association
+    {
+        public long Id { get; set; }
+        public string AssociationName { get; set; }
+
+        private Random random = new Random();
+
+        public void InsertRowsToTable()
+        {
+            var associations = GetAssociations();
+            using (
+                SqlConnection connection =
+                    new SqlConnection(
+                        @"Data Source =RAHULPC\SQLEXPRESS;Initial Catalog=TripleStore;Integrated Security=True;MultipleActiveResultSets=true;")
+            )
+            {
+                int tally = connection.Execute(@"insert Associations (Id, AssociationName) values(@Id, @AssociationName)",
+                    associations);
+
+                Console.WriteLine(tally);
+            }
+        }
+
+        public List<Association> GetAssociations()
+        {
+            var ids = File.ReadAllLines(@"C:\Rahul\associations.txt");
+            var associations = new List<Association>();
+            foreach (var id in ids)
+            {
+                var associationName = RandomWordGenerator(new Random().Next(4, 8));
+                while (associations.Any(p => p.AssociationName == associationName))
+                {
+                    associationName = RandomWordGenerator(new Random().Next(4, 8));
+                }
+                var association = new Association
+                {
+                    AssociationName = associationName,
+                    Id = Convert.ToInt64(id)
+                };
+                associations.Add(association);
+            }
+            return associations;
+        }
+
+        private string RandomWordGenerator(int requestedLength)
+        {
+            Random rnd = new Random();
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            string[] vowels = { "a", "e", "i", "o", "u" };
+
+            string word = "";
+
+            if (requestedLength == 1)
+            {
+                word = GetRandomLetter(rnd, vowels);
+            }
+            else
+            {
+                for (int i = 0; i < requestedLength; i += 2)
+                {
+                    word += GetRandomLetter(rnd, consonants) + GetRandomLetter(rnd, vowels);
+                }
+
+                word = word.Replace("q", "qu").Substring(0, requestedLength); // We may generate a string longer than requested length, but it doesn't matter if cut off the excess.
+            }
+
+            return word.ToUpperInvariant();
+        }
+
+        private string GetRandomLetter(Random rnd, string[] letters)
+        {
+            return letters[rnd.Next(0, letters.Length - 1)];
+        }
+
+        private List<int> GenerateRandom(int count)
+        {
+            // generate count random values.
+            HashSet<int> candidates = new HashSet<int>();
+            while (candidates.Count < count)
+            {
+                // May strike a duplicate.
+                candidates.Add(random.Next());
+            }
+
+            // load them in to a list.
+            List<int> result = new List<int>();
+            result.AddRange(candidates);
+
+            // shuffle the results:
+            int i = result.Count;
+            while (i > 1)
+            {
+                i--;
+                int k = random.Next(i + 1);
+                int value = result[k];
+                result[k] = result[i];
+                result[i] = value;
+            }
+            return result;
+        }
+    }
+}
---
+++ DataLoader/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DataLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlLoader sqlLoader = new SqlLoader();
+            var triples = new TripleStore().GetTriples();
+            string connectionString = @"Data Source =RAHULPC\SQLEXPRESS;Initial Catalog=TripleStore;Integrated Security=True;MultipleActiveResultSets=true;";
+            sqlLoader.Load(connectionString, triples);
+            Console.WriteLine("Done inserting");
+            Console.Read();
+        }
+    }
+
+    public class TripleStore
+    {
+
+
+        public DataTable GetTriples()
+        {
+            Random rnd = new Random();
+            DataTable dataTable = new DataTable("Triples");
+
+            dataTable.Columns.Add("OrderId");
+            dataTable.Columns.Add("Association");
+            dataTable.Columns.Add("ObjectId");
+
+            List<string> associationIds = File.ReadAllLines(@"C:\Rahul\associations.txt").ToList();
+            List<Triple> triples = new List<Triple>();
+            for (int i = 300000; i <= 400000; i++)
+            {
+                var values = new object[3];
+                foreach (var associationId in associationIds)
+                {
+                    Console.Write("Making Triples: {0}\r", dataTable.Rows.Count);
+                    values[0] = i + 23400;
+                    values[1] = Convert.ToInt64(associationId);
+                    values[2] = 1000 + i;
+
+                    dataTable.Rows.Add(values);
+                }
+            }
+            Console.WriteLine("Done getting triples object!");
+            return dataTable;
+        }
+
+    }
+
+    public class Triple
+    {
+        public long OrderId { get; set; }
+        public long Association { get; set; }
+        public long ObjectId { get; set; }
+    }
+
+
+}
